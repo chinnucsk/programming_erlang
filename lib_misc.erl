e9c76bc382c0043bc125ba43598d986b262b14e5
David Xia <david@davidxia.com>
2012-06-21 00:17:08 -0400

    Finished chapter 4

--- lib_misc.erl
+++ lib_misc.erl
@@ -1,19 +1,23 @@
 -module(lib_misc).
 -compile(export_all).
 
+
 sum(L) -> sum(L, 0).
 
 sum([], N) -> N;
 sum([H | T], N) -> sum(T, H + N).
 
+
 for(Max, Max, F) -> [F(Max)];
 for(I, Max, F)   -> [F(I) | for(I + 1, Max, F)].
+
 
 qsort([]) -> [];
 qsort([Pivot | T]) ->
         qsort([X || X <- T, X < Pivot])
         ++ [Pivot] ++
         qsort([X || X <- T, X >= Pivot]).
+
 
 pythag(N) ->
     [ {A, B, C} ||
@@ -24,6 +28,30 @@ pythag(N) ->
         A * A + B * B =:= C * C
     ].
 
+
 perms([]) -> [[]];
 perms(L)  -> [[H | T] || H <- L, T <- perms(L -- [H])].
+
+
+odds_and_evens(L) ->
+    Odds  = [X || X <- L, (X rem 2) =:= 1],
+    Evens = [X || X <- L, (X rem 2) =:= 0],
+    {Odds, Evens}.
+
+odds_and_evens_acc(L) ->
+    odds_and_evens_acc(L, [], []).
+
+odds_and_evens_acc([H | T], Odds, Evens) ->
+    case (H rem 2) of
+        1 -> odds_and_evens_acc(T, [H | Odds], Evens);
+        0 -> odds_and_evens_acc(T, Odds, [H | Evens])
+    end;
+odds_and_evens_acc([], Odds, Evens) ->
+    {Odds, Evens}.
+
+
+sqrt(X) when X < 0 ->
+    erlang:error({squareRootNegativeArgument, X});
+sqrt(X) ->
+    math:sqrt(X).
 
