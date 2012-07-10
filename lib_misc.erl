0911ae8160197b8110536bcceba8264767fe0be6
David Xia <david@davidxia.com>
2012-07-10 17:29:19 -0400

    Done with chapter 8.

--- lib_misc.erl
+++ lib_misc.erl
@@ -55,3 +55,31 @@ sqrt(X) when X < 0 ->
 sqrt(X) ->
     math:sqrt(X).
 
+
+sleep(T) ->
+    receive
+    after T ->
+            true
+    end.
+
+
+flush_buffer() ->
+    receive
+        _Any ->
+            flush_buffer()
+    after 0 ->
+            true
+    end.
+
+
+priority_receive() ->
+    receive
+        {alarm, X} ->
+            {alarm, X}
+    after 0 ->
+            receive
+                Any ->
+                    Any
+            end
+    end.
+
